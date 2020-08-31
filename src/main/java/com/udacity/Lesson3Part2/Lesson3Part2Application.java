1d72008a2d30567b27d2a1d1fc9230dfa69899f9
ShannonHung <b10709036@mail.ntust.edu.tw>
2020-08-31 11:48:05 +0800

    start

---
+++ src/main/java/com/udacity/Lesson3Part2/Lesson3Part2Application.java
@@ -0,0 +1,13 @@
+package com.udacity.Lesson3Part2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson3Part2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson3Part2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/udacity/Lesson3Part2/Lesson3Part2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.Lesson3Part2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson3Part2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
